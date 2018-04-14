80849db5bd0460b644c47903b7aa7394b39af89f
Frederick Akalin <akalin@keyba.se>
2018-04-13 18:57:40 -0700

    Rem unused

--- shared/constants/teams.js
+++ shared/constants/teams.js
@@ -182,9 +182,6 @@ const getChannelInfoFromConvID = (state: TypedState, conversationIDKey: ChatType
 const getChannelNameFromConvID = (state: TypedState, conversationIDKey: ChatTypes.ConversationIDKey) =>
   state.teams.convIDToChannelInfo.getIn([conversationIDKey, 'channelname'], null)
 
-const getTopicFromConvID = (state: TypedState, conversationIDKey: ChatTypes.ConversationIDKey) =>
-  state.teams.convIDToChannelInfo.getIn([conversationIDKey, 'description'], null)
-
 const getRole = (state: TypedState, teamname: Types.Teamname): Types.MaybeTeamRoleType =>
   state.teams.getIn(['teamNameToRole', teamname], 'none')
 
@@ -385,7 +382,6 @@ export {
   getTeamRequests,
   getTeamConvIDs,
   getSortedTeamnames,
-  getTopicFromConvID,
   getTeamType,
   isAdmin,
   isBigTeam,
